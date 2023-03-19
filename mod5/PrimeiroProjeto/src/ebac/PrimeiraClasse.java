dac0644d3682753c3d2c08bf6979453732dc5158
Clb35 <124786710+Clb35@users.noreply.github.com>
2023-03-19 11:21:05 -0300

    Add files via upload

---
+++ mod5/PrimeiroProjeto/src/ebac/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package ebac;
+
+public class PrimeiraClasse {
+	public static void main(String args[]) {
+		String Ambiente = args[0];
+		System.out.println("Hello " +Ambiente);			
+	
+	}
+
+}
---
+++ mod5/PrimeiroProjeto/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Cassiano
+ *
+ */
+module PrimeiroProjeto {
+}
